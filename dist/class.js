35720dcb32552329674bd15577d3dbb9015cff43
sufihasan <hasan6nh@gmail.com>
2025-10-26 08:48:20 +0600

    make constructor inside a class

--- dist/class.js
+++ dist/class.js
@@ -11,11 +11,13 @@ class Person {
         console.log(`welcome ${this.name} your age is ${this.age}`);
     }
 }
+// Creating instances (objects) from the class
 const user = new Person();
 user.name = 'Hasan';
 user.age = 27;
-user.greet();
+user.greet(); // Calling a method // output welcome Hasan your age is 27
+// Creating instances (objects) from the class
 const user2 = new Person();
 user2.name = 'salman';
 user2.age = 30;
-user2.greet();
+user2.greet(); // Calling a method // output welcome salman your age is 30
--- dist/constractorAndModifier.js
+++ dist/constractorAndModifier.js
@@ -1,20 +1,17 @@
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 class Person {
-    name: string;
-    age: number;
-
-    constructor(nameP: string, ageP: number) {
+    name;
+    age;
+    constructor(nameP, ageP) {
         this.name = nameP;
         this.age = ageP;
     }
-
     greet() {
         console.log(`welcome ${this.name} and your age is ${this.age}`);
     }
 }
-
-
 // create and instance (object ) of Person class
 // when we make instance (object) of a class, its constructor call automatically 
 const user1 = new Person('Hasan', 27);
-user1.greet();   // call greet via object
+user1.greet();
